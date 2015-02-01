93fc2c770abc8da1ca2cd7010dc3cd76f3309f53
velusgautam <gautam@wixzi.com>
2015-02-01 21:34:49 +0530

    DB Configs
    DBConfigs Depends on your LAMP server.

---
+++ Database/dbConfig.php
@@ -0,0 +1,21 @@
+<?php
+	/**
+	 * User: Velu S Gautam
+	 * Date: 9/8/13
+	 * Time: 10:10 AM
+	 */
+if (!defined("_VALID_PHP"))
+		die('Direct access to this location is not allowed.');
+//database server
+	define('DB_SERVER', "localhost");
+//database name
+	define('DB_DATABASE', "AjaxTreeView");
+//database login name
+	define('DB_USER', "root");
+//database login password
+	define('DB_PASS', "");
+
+//Define your table names also
+	define('TABLE_DATA', "user-data");
+
+?>
