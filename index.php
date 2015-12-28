a115459f5037887645de537b1e239f41624a6fcf
Velu S Gautam <i@velusgautam.com>
2015-12-28 17:02:45 +0530

    Bootstrap
    Included HTML and BootStrap files to Index Page

--- index.php
+++ index.php
@@ -2,12 +2,22 @@
 	/* 	Project : PHP Ajax:  Tree View
 		Developer : Velu S Gautam
 		Start Date : 31-01-2015
-		Last Modified : 06-02-2015
+		Last Modified : 06-12-2015
 	*/
 	
 	$db = ""; /*Database Details*/
 	$data = "";
 ?>
+<html>
+<head>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+
+</head>
+<body>
 <table class="table">
 	<tr>
 		<td class="col-lg-8">Name</td>
@@ -18,3 +28,7 @@
 </table>
 <?php
 ?>
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+</body>
+</html>
